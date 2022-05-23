989e108d0b3ef32690edb8f31d2abef4b8a2662a
Josh Sutheran <josh.sutheran@gmail.com>
2022-05-23 14:19:59 +0100

    Added button icons

---
+++ src/constants/images.js
@@ -0,0 +1,21 @@
+import clear from '../images/c--icon.png'
+import del from '../images/clear--icon.png'
+import divide from '../images/divide--icon.png'
+import dot from '../images/dot--icon.png'
+import multiply from '../images/multiply--icon.png'
+import equal from '../images/equal--icon.png'
+import percent from '../images/percent--icon.png'
+import plus from '../images/plus--icon.png'
+import minus from '../images/subtraction--icon.png'
+
+export default {
+    clear,
+    del,
+    divide,
+    dot,
+    multiply,
+    equal,
+    percent,
+    plus,
+    minus
+}
